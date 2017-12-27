4147e4ac387523b0c12c3943f1ee47bbd2894c7a
EvilPort2 <dibakarsaha1234@gmail.com>
2017-12-27 20:49:44 +0530

    Better gesture_api

--- gesture_api.py
+++ gesture_api.py
@@ -4,6 +4,72 @@ import time
 import datetime
 import cv2
 import thread
+from threading import Thread
+
+class TakePhoto(Thread):
+	def __init__(self, cam):
+		Thread.__init__(self)
+		self.cam = cam
+		self.start()
+
+	def run(self):
+		cam = self.cam
+		time.sleep(5)
+		img1 = cam.read()[1]
+		ts = time.time()
+		st = datetime.datetime.fromtimestamp(ts).strftime('%Y_%m_%d %H_%M_%S')
+		cv2.imwrite("photos/"+st + ".png", img1)
+		
+
+def screenshot(x):
+	ts = time.time()
+	st = datetime.datetime.fromtimestamp(ts).strftime('%Y_%m_%d %H_%M_%S')
+	gui.screenshot("screenshot/" + st + ".png")
+
+def text_editor(x = None):
+	thread.start_new_thread(os.system, ("notepad", ))
+
+def start_menu(x = None):
+	gui.press('winleft')
+
+def new_file(x = None):
+	gui.hotkey('ctrlleft', 'n')
+
+def select_all(x = None):
+	gui.hotkey('ctrlleft', 'a')
+
+def close(x = None):
+	gui.hotkey('altleft', 'f4')
+
+def copy(x = None):
+	gui.hotkey('ctrlleft', 'c')
+	
+def paste(x = None):
+	gui.hotkey('ctrlleft', 'v')
+
+def cut(x = None):
+	gui.hotkey('ctrlleft', 'x')
+
+def next_window(x = None):
+	gui.hotkey('altleft', 'tab')
+
+def prev_window(x = None):
+	gui.hotkey('altleft', 'shiftleft', 'tab')
+
+def maximize(x = None):
+	gui.hotkey('winleft', 'up')
+
+def minimize(x = None):
+	gui.hotkey('winleft', 'down')
+
+def lockscreen(cam):
+	cam.release()
+	os.system("python modules/face_lock_unlock/unlock_using_face.py")
+	cam = cv2.VideoCapture(1)
+	return cam
+
+def task_manager(x = None):
+	gui.hotkey('ctrlleft', 'shiftleft', 'esc')
 
 GEST_START = ("N", "E", "S", "W")
 GEST_CLOSE = ("SE", "N", "SW")
@@ -12,67 +78,48 @@ GEST_PASTE = ("SE", "NE")
 GEST_CUT = ("SW", "N", "SE")
 GEST_ALT_TAB = ("SE", "SW")
 GEST_ALT_SHIFT_TAB = ("SW", "SE")
-GEST_MAXIMISE = ("N",)
-GEST_MINIMISE = ("S",)
+GEST_MAXIMIZE = ("N",)
+GEST_MINIMIZE = ("S",)
 GEST_LOCK = ("S", "E")
 GEST_TASK_MANAGER = ("E", "W", "S")
 GEST_NEW_FILE = ("N", "SE", "N")
 GEST_SELECT_ALL = ("NE", "SE", "NW", "W")
-GEST_SHOW_PIC1 = ("W", "S", "E", "S", "W")
-GEST_SHOW_PIC2 = ("E", "S", "W", "E", "N")
+
+GESTURES_ONE_HAND = \
+{GEST_START: start_menu, 
+GEST_CLOSE: close,
+GEST_COPY: copy,
+GEST_PASTE: paste,
+GEST_CUT: cut,
+GEST_ALT_TAB: next_window,
+GEST_ALT_SHIFT_TAB: prev_window,
+GEST_MAXIMIZE: maximize,
+GEST_MINIMIZE: minimize,
+GEST_LOCK: lockscreen,
+GEST_TASK_MANAGER: task_manager,
+GEST_NEW_FILE: new_file,
+GEST_SELECT_ALL: select_all}
+
+
 GEST_SCREENSHOT = (("W", "S", "E"), ("E", "S", "W"))
 GEST_CAMERA = (("SW", "SE"), ("SE", "SW"))
 GEST_TEXT_EDITOR = (("N", "E", "S", "W"), ("S",))
 
-GESTURES_ONE_HAND = {GEST_CUT: ('ctrlleft', 'x'),
-GEST_CLOSE: ('altleft', 'f4'),
-GEST_ALT_SHIFT_TAB: ('altleft', 'shiftleft', 'tab'),
-GEST_PASTE: ('ctrlleft', 'v'),
-GEST_ALT_TAB: ('altleft', 'tab'),
-GEST_COPY: ('ctrlleft', 'c'),
-GEST_NEW_FILE: ('ctrlleft', 'n'),
-GEST_SELECT_ALL: ('ctrlleft', 'a')}
-
-
-def screenshot():
-    ts = time.time()
-    st = datetime.datetime.fromtimestamp(ts).strftime('%Y_%m_%d %H_%M_%S')
-    gui.screenshot("screenshot/" + st + ".png")
-
-def text_editor():
-    print("text_editor")
-    if os.name == 'nt':
-        os.system("notepad.exe")
-    else:
-        os.system("gedit")
-
-GESTURES_TWO_HAND = {GEST_SCREENSHOT: screenshot,
+GESTURES_TWO_HAND = \
+{GEST_SCREENSHOT: screenshot,
+GEST_CAMERA: TakePhoto,
 GEST_TEXT_EDITOR: text_editor}
 
-if os.name == 'nt':
-    GESTURES_ONE_HAND[GEST_START] = ('winleft',)
-    GESTURES_ONE_HAND[GEST_LOCK] = ('winleft', 'l')
-    GESTURES_ONE_HAND[GEST_TASK_MANAGER] = ('ctrlleft', 'shiftleft', 'esc')
-else:
-    GESTURES_ONE_HAND[GEST_START] = ('altleft', 'f1')
-    GESTURES_ONE_HAND[GEST_LOCK] = ('ctrlleft', 'altleft', 'l')
-    GESTURES_ONE_HAND[GEST_TASK_MANAGER] = ('ctrlleft', 'esc')
+def do_gesture_action(cam, gesture1, gesture2 = None):
+	ret = None
+	if gesture2 == None:
+		if gesture1 in GESTURES_ONE_HAND.keys():
+			ret = GESTURES_ONE_HAND[gesture1](cam)
+	else:
+		if (gesture1, gesture2) in GESTURES_TWO_HAND.keys():
+			ret = GESTURES_TWO_HAND[(gesture1, gesture2)](cam)
 
-def do_gesture_action(gesture1, gesture2 = None):
-    print(gesture1)
-    print(gesture2)
-    if gesture2 == None:
-        if gesture1 in GESTURES_ONE_HAND.keys():
-            keys = list(GESTURES_ONE_HAND[gesture1])
-            last_key = keys.pop()
-            if len(keys) >= 1:
-                for key in keys:
-                    gui.keyDown(key)
-            gui.press(last_key)
-            if len(keys) >= 1:
-                keys.reverse()
-                for key in keys:
-                    gui.keyUp(key)
-    else:
-        if (gesture1, gesture2) in GESTURES_TWO_HAND.keys():
-            thread.start_new_thread(GESTURES_TWO_HAND[(gesture1, gesture2)], ())
+	if ret != None:
+		cam = ret
+
+	return cam
